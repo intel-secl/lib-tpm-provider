c4840b490801eb30653ad4bf61883a4b32cc4da6
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2019-04-17 12:21:08 +0530

    add quote tss implementation

---
+++ tpm-provider/src/main/java/com/intel/mtwilson/core/tpm/util/PcrBanksMapper.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2019 Intel Corporation
+ * All rights reserved.
+ */
+
+package com.intel.mtwilson.core.tpm.util;
+
+import com.intel.mtwilson.core.tpm.Tpm;
+import tss.tpm.TPM_ALG_ID;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class PcrBanksMapper {
+    public static List<TPM_ALG_ID> getSupportedPcrBanks(Set<Tpm.PcrBank> pcrBanks) {
+        List<TPM_ALG_ID> supportedBanks = new ArrayList<>();
+        for(Tpm.PcrBank pb: pcrBanks) {
+            switch(pb) {
+                case SHA1:
+                    supportedBanks.add(TPM_ALG_ID.SHA1);
+                    break;
+                case SHA256:
+                    supportedBanks.add(TPM_ALG_ID.SHA256);
+                    break;
+                case SHA384:
+                    supportedBanks.add(TPM_ALG_ID.SHA384);
+                    break;
+                case SHA512:
+                    supportedBanks.add(TPM_ALG_ID.SHA512);
+                    break;
+                case SM3_256:
+                    supportedBanks.add(TPM_ALG_ID.SM3_256);
+                    break;
+                default:
+                    throw new RuntimeException("PCR Bank not supported yet");
+            }
+        }
+        return supportedBanks;
+    }
+}
