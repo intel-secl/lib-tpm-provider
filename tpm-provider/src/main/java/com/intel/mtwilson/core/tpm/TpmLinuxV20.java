52c687a7b0b7cffc6c0706c1d7c283fe0a2036f8
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2019-05-11 09:49:21 -0700

    Fix windows isOwned functionality

--- tpm-provider/src/main/java/com/intel/mtwilson/core/tpm/TpmLinuxV20.java
+++ tpm-provider/src/main/java/com/intel/mtwilson/core/tpm/TpmLinuxV20.java
@@ -4,10 +4,15 @@
  */
 package com.intel.mtwilson.core.tpm;
 
+import com.intel.mtwilson.core.tpm.model.PersistentIndex;
 import gov.niarl.his.privacyca.TpmUtils;
 import tss.TpmDeviceBase;
+import tss.tpm.*;
+
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -18,6 +23,41 @@ class TpmLinuxV20 extends TpmV20 {
 
     TpmLinuxV20(TpmDeviceBase base) {
         super(base);
+    }
+
+    @Override
+    public void takeOwnership(byte[] newOwnerAuth) throws IOException, Tpm.TpmException {
+        changeAuth(newOwnerAuth);
+
+        // Create an RSA storage key in the owner hierarchy. This is
+        // conventionally called an SRK
+        TPMT_PUBLIC inPublic = new TPMT_PUBLIC(TPM_ALG_ID.SHA256,
+                new TPMA_OBJECT(TPMA_OBJECT.restricted, TPMA_OBJECT.userWithAuth, TPMA_OBJECT.decrypt,
+                        TPMA_OBJECT.fixedTPM, TPMA_OBJECT.fixedParent, TPMA_OBJECT.sensitiveDataOrigin),
+                new byte[0],
+                new TPMS_RSA_PARMS(new TPMT_SYM_DEF_OBJECT(TPM_ALG_ID.AES, 128, TPM_ALG_ID.CFB),
+                        new TPMS_NULL_ASYM_SCHEME(),2048,0),
+                new TPM2B_PUBLIC_KEY_RSA());
+
+        CreatePrimaryResponse cpResponse;
+        TPM_HANDLE oHandle = getOwnerHandle(newOwnerAuth);
+        try {
+            cpResponse = tpm.CreatePrimary(oHandle,
+                    new TPMS_SENSITIVE_CREATE(), inPublic, new byte[0], new TPMS_PCR_SELECTION[0]);
+        } catch (tss.TpmException e) {
+            log.error("Failed to create storage primary key");
+            throw new Tpm.TpmException("Failed to create storage primary key");
+        }
+
+        try {
+            tpm.EvictControl(oHandle, cpResponse.handle,
+                    TPM_HANDLE.from(PersistentIndex.PK.getValue()));
+        } catch (tss.TpmException e) {
+            if (!e.getMessage().contains("NV_DEFINED")) {
+                log.error("Failed to make storage primary key persistent");
+                throw new Tpm.TpmException("Failed to make storage primary key persistent");
+            }
+        }
     }
 
     @Override
@@ -68,6 +108,54 @@ class TpmLinuxV20 extends TpmV20 {
     @Override
     public String getTcbMeasurement() throws IOException, TpmException {
         return new TpmLinuxV12(null).getTcbMeasurement();
+    }
+
+    /**
+     *
+     * @param ownerAuth
+     * @return
+     * @throws IOException
+     */
+    @Override
+    public boolean isOwnedWithAuth(byte[] ownerAuth) throws IOException {
+        return changeAuth(ownerAuth, ownerAuth);
+    }
+
+    private boolean changeAuth(byte[] oldAuth, byte[] newAuth) {
+        Set<TPM_HANDLE> handles = new HashSet<>(Arrays.asList(TPM_HANDLE.from(TPM_RH.OWNER),
+                TPM_HANDLE.from(TPM_RH.ENDORSEMENT), TPM_HANDLE.from(TPM_RH.LOCKOUT)));
+
+        for (TPM_HANDLE handle : handles) {
+            if (oldAuth != null) {
+                handle.AuthValue = oldAuth;
+            }
+            try {
+                tpm.HierarchyChangeAuth(handle, newAuth);
+            } catch (tss.TpmException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void changeAuth(byte[] ownerAuth) throws IOException, Tpm.TpmException {
+        // take ownership and see if we can change it and revert it from a temporary
+        if (!changeAuth(null, ownerAuth)) {
+            byte[] newOwnerPass = TpmUtils.createRandomBytes(20);
+            if (!changeAuth(ownerAuth, newOwnerPass)) {
+                // supplied newOwnerAuth is invalid
+                log.error("Cannot take ownership; TPM claimed with a different password");
+                throw new Tpm.TpmException("Cannot take ownership; TPM claimed with a different password");
+            } else {
+                // supplied newOwnerAuth is valid, so change TPM owner pass back from the temporary and do it again
+                if (!changeAuth(newOwnerPass, ownerAuth)) {
+                    log.error("CRITICAL ERROR: Could not change TPM password back from temporary. TPM must be reset from bios");
+                    throw new Tpm.TpmException("CRITICAL ERROR: "
+                            + "Could not change TPM password back from temporary. TPM must be reset from bios");
+                }
+            }
+        }
     }
 
     private int getECIndex() {
