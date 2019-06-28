0489d8f4ead85ef71add46f6edcd09c913ce9b02
Rawat, Arvind <arvind.rawat@intel.com>
2019-06-28 09:25:50 -0700

    Added copyright headers to source files.

--- tpm-provider/src/main/java/com/intel/mtwilson/core/tpm/TpmV20.java
+++ tpm-provider/src/main/java/com/intel/mtwilson/core/tpm/TpmV20.java
@@ -1,8 +1,7 @@
 /*
  * Copyright (C) 2019 Intel Corporation
- * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
  */
-
 package com.intel.mtwilson.core.tpm;
 
 import com.intel.mtwilson.core.common.tpm.model.IdentityProofRequest;
